23c7f26541c1d9322ba0c7e6f034df5ee6bc832a
Duncan MacLennan <dcupmusic@gmail.com>
2023-12-03 18:51:55 +1100

    more fixes

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,7 +15,7 @@ class test_basemodel(unittest.TestCase):
         """ """
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
-        self.value = BaseModel
+        self.value = User
 
     def setUp(self):
         """ """
@@ -29,27 +29,27 @@ class test_basemodel(unittest.TestCase):
 
     def test_default(self):
         """ """
-        i = self.value()
+        i = self.value()  # replaced BaseModel with User
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
         """ """
-        i = self.value()
+        i = self.value()  # replaced BaseModel with User
         copy = i.to_dict()
-        new = BaseModel(**copy)
+        new = User(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
         """ """
-        i = self.value()
+        i = self.value()  # replaced BaseModel with User
         copy = i.to_dict()
         copy.update({1: 2})
         with self.assertRaises(TypeError):
-            new = BaseModel(**copy)
+            new = User(**copy)
 
     def test_save(self):
         """ Testing save """
-        i = self.value()
+        i = self.value()  # replaced BaseModel with User
         i.save()
         key = self.name + "." + i.id
         with open('file.json', 'r') as f:
@@ -58,13 +58,13 @@ class test_basemodel(unittest.TestCase):
 
     def test_str(self):
         """ """
-        i = self.value()
+        i = self.value()  # replaced BaseModel with User
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
                          i.__dict__))
 
     def test_todict(self):
         """ """
-        i = self.value()
+        i = self.value()  # replaced BaseModel with User
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
@@ -95,5 +95,5 @@ class test_basemodel(unittest.TestCase):
     #     new = self.value()
     #     self.assertEqual(type(new.updated_at), datetime.datetime)
     #     n = new.to_dict()
-    #     new = BaseModel(**n)
+    #     new = User(**n)
     #     self.assertFalse(new.created_at == new.updated_at)
