0bf6b12c4ed7ed9c77f5c54889e8f6fc15f8a252
Duncan MacLennan <dcupmusic@gmail.com>
2023-12-03 18:18:30 +1100

    fix amenity and test amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
 """
-    State Module for HBNB project
-    *Update 1/12/2023: added code to use db storage and associated imports
-        part of question 10
+    Amenity Module for HBNB project
 """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy import Column, String, Table
 from sqlalchemy.orm import relationship
-import os
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+
         place_amenities = Table(
             'place_amenity', Base.metadata,
-            Column('place_id', String(60), ForeignKey('places.id'),
-                   primary_key=True, nullable=False),
-            Column('amenity_id', String(60), ForeignKey('amenities.id'),
-                   primary_key=True, nullable=False)
+            Column('place_id', String(60), ForeignKey(
+                'places.id'), primary_key=True, nullable=False),
+            Column('amenity_id', String(60), ForeignKey(
+                'amenities.id'), primary_key=True, nullable=False)
         )
-        name = Column(String(128), nullable=False)
-        places = relationship('Place', secondary='place_amenity',
-                              back_populates='amenities')
-    else:
-        name = ""
+
+        places = relationship(
+            'Place', secondary=place_amenities, back_populates='amenities')
--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-""" """
+"""Test suite for the Amenity class."""
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """Defines test cases for the Amenity class."""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initializes the test suite."""
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
-        new = self.value()
+        """Tests that the name attribute of an Amenity instance is a string."""
+        new = self.value(name="WiFi")
         self.assertEqual(type(new.name), str)
+
+        self.assertIsNotNone(new.name)
+        self.assertNotEqual(new.name, "")
