24f1651cdc8bdfb40afa5e19eac9fa45da5e3b71
Duncan MacLennan <dcupmusic@gmail.com>
2023-12-03 19:33:37 +1100

    test file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ Module for testing file storage"""
 import unittest
+from models.base_model import BaseModel
 from models import storage
 import os
-from models.engine.file_storage import FileStorage
-from models.user import User
 
 
 class test_fileStorage(unittest.TestCase):
@@ -29,25 +28,46 @@ class test_fileStorage(unittest.TestCase):
         """ __objects is initially empty """
         self.assertEqual(len(storage.all()), 0)
 
+    # def test_new(self):
+    #     """ New object is correctly added to __objects """
+    #     new = BaseModel()
+    #     for obj in storage.all().values():
+    #         temp = obj
+    #     self.assertTrue(temp is obj)
+
     def test_all(self):
         """ __objects is properly returned """
-        new = User()  # Use a concrete subclass instead of BaseModel
+        new = BaseModel()
         temp = storage.all()
         self.assertIsInstance(temp, dict)
 
+    def test_base_model_instantiation(self):
+        """ File is not created on BaseModel save """
+        new = BaseModel()
+        self.assertFalse(os.path.exists('file.json'))
+
     def test_empty(self):
         """ Data is saved to file """
-        new = User()  # Use a concrete subclass instead of BaseModel
+        new = BaseModel()
         thing = new.to_dict()
         new.save()
-        new2 = User(**thing)  # Use a concrete subclass instead of BaseModel
+        new2 = BaseModel(**thing)
         self.assertNotEqual(os.path.getsize('file.json'), 0)
 
     def test_save(self):
         """ FileStorage save method """
-        new = User()  # Use a concrete subclass instead of BaseModel
+        new = BaseModel()
         storage.save()
         self.assertTrue(os.path.exists('file.json'))
+
+    # def test_reload(self):
+    #     """ Storage file is successfully loaded to __objects """
+    #     new = BaseModel()
+    #     storage.save()
+    #     storage.reload()
+    #     for obj in storage.all().values():
+    #         loaded = obj
+    #     self.assertEqual(new.to_dict()['id'], loaded.to_dict()['id'])
 
     def test_reload_empty(self):
         """ Load from an empty file """
@@ -60,6 +80,12 @@ class test_fileStorage(unittest.TestCase):
         """ Nothing happens if file does not exist """
         self.assertEqual(storage.reload(), None)
 
+    def test_base_model_save(self):
+        """ BaseModel save method calls storage save """
+        new = BaseModel()
+        new.save()
+        self.assertTrue(os.path.exists('file.json'))
+
     def test_type_path(self):
         """ Confirm __file_path is string """
         self.assertEqual(type(storage._FileStorage__file_path), str)
@@ -68,7 +94,16 @@ class test_fileStorage(unittest.TestCase):
         """ Confirm __objects is a dict """
         self.assertEqual(type(storage.all()), dict)
 
+    # def test_key_format(self):
+    #     """ Key is properly formatted """
+    #     new = BaseModel()
+    #     _id = new.to_dict()['id']
+    #     for key in storage.all().keys():
+    #         temp = key
+    #     self.assertEqual(temp, 'BaseModel' + '.' + _id)
+
     def test_storage_var_created(self):
         """ FileStorage object storage created """
+        from models.engine.file_storage import FileStorage
         print(type(storage))
         self.assertEqual(type(storage), FileStorage)
