11d64ba4b23e40c4b47088e8854d09544a0749a2
Duncan MacLennan <dcupmusic@gmail.com>
2023-12-03 17:24:56 +1100

    ammended models/state, added init

--- models/state.py
+++ models/state.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import os  # double check circular import
+import os
 
 
 class State(BaseModel, Base):
     """
-        State class
+    State class
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state', cascade='all, delete')
+    else:
         @property
         def cities(self):
-            """return list of City instances where state_id = State.id"""
-            from models import storage
+            """ Return the list of City instances where state_id equals State.id """
             from models.city import City
-            cities_list = []
-            for city in storage.all(City).values():
-                if city.state_id == self.id:
-                    cities_list.append(city)
-            return cities_list
+            city_list = [city for city in storage.all(
+                City).values() if city.state_id == self.id]
+            return city_list
 
-        cities = relationship('City', backref='state', cascade='all, delete')
+    def __init__(self, *args, **kwargs):
+        """Initialization for the State class."""
+        super().__init__(*args, **kwargs)
+        self.name = kwargs.get('name', "")
