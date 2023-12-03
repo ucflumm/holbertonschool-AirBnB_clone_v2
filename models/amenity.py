81aabc7a061a7d6070f2fc79a005a01d4562a8c3
Duncan MacLennan <dcupmusic@gmail.com>
2023-12-03 18:45:48 +1100

    more fixes

--- models/amenity.py
+++ models/amenity.py
@@ -4,7 +4,7 @@
 """
 import os
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Table
+from sqlalchemy import Column, String, Table, ForeignKey
 from sqlalchemy.orm import relationship
 
 
