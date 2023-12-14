ece8b34c850537827a8bf9952abf2bee418c8270
ucflumm <ucflumm@gmail.com>
2023-12-14 13:47:01 +1100

    Add Flask routes for number and text variables

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+    Write a script that starts a Flask web application:
+
+    Your web application must be listening on 0.0.0.0 port 5000
+        Routes:
+        /: display “Hello HBNB!”
+        /hbnb: display “HBNB”
+        /c/<text>: display “C ” followed by the value of the text variable
+        (replace underscore _ symbols with a space )
+        /python/(<text>): display “Python ”, followed by the value of the
+        text variable (replace underscore _ symbols with a space )
+            The default value of text is “is cool”
+        /number/<n>: display “n is a number” only if n is an integer
+    You must use the option strict_slashes=False in your route definition
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Says Hello on the home page"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Says HBNB on the /hbnb page"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Says C followed by the text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """Says Python followed by the text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Says n is a number only if n is an integer"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
