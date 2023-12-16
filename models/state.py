3d6b8b6cf54bc4284b2c8373fb63e98ee2ae581c
ucflumm <ucflumm@gmail.com>
2023-12-16 12:12:35 +1100

    Refactor State class to improve code readability and maintainability

--- models/state.py
+++ models/state.py
@@ -9,19 +9,20 @@ import os
 
 class State(BaseModel, Base):
     """
-    State class
+        State class inherits from BaseModel and Base
     """
     __tablename__ = "states"
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship('City', back_populates='state', cascade='delete, delete-orphan')
+        cities = relationship('City', back_populates='state',
+                              cascade='delete, delete-orphan')
     else:
         @property
         def cities(self):
             """
-                getter attribute cities that returns the list of City instances
-                with state_id equals to the current State.id
+                Returns the list of City instances with state_id equals
+                to the current State.id
             """
             from models import storage
             from models.city import City
