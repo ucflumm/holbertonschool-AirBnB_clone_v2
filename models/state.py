c3be7f9aee059027fe2487f7946ef19a9f5b4809
ucflumm <ucflumm@gmail.com>
2023-12-16 11:32:43 +1100

    Fix relationships and add getter attribute for cities in State model

--- models/state.py
+++ models/state.py
@@ -14,18 +14,18 @@ class State(BaseModel, Base):
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='all, delete')
+        cities = relationship('City', back_populates='state', cascade='delete, delete-orphan')
     else:
-        name = ""
-
         @property
         def cities(self):
+            """
+                getter attribute cities that returns the list of City instances
+                with state_id equals to the current State.id
+            """
+            from models import storage
             from models.city import City
-            city_list = [city for city in storage.all(
-                City).values() if city.state_id == self.id]
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
             return city_list
-
-    # def __init__(self, *args, **kwargs):
-    #     """Initialization for the State class."""
-    #     super().__init__(*args, **kwargs)
-    #     self.name = kwargs.get('name', "")
