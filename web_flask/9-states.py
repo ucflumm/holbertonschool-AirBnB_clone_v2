764e1d585e5b8243720eac1c99e8544ce3530aa5
ucflumm <ucflumm@gmail.com>
2023-12-16 14:13:51 +1100

    Add routes and templates for displaying states and cities

---
+++ web_flask/9-states.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+    10. States and State
+    Routes:
+        /states: display a HTML page: (inside the tag BODY)
+            H1 tag: “States”
+            UL tag: with the list of all State objects present in DBStorage
+                LI tag: description of one State: <state.id>: <B><state.name></B>
+        /states/<id>: display a HTML page: (inside the tag BODY)
+            If a State object is found with this id:
+                H1 tag: “State: ”
+                H3 tag: “Cities:”
+                UL tag: with the list of City objects linked to the State
+                    LI tag: description of one City: <city.id>: <B><city.name></B>
+            Otherwise:
+                H1 tag: “Not found!”
+"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exc):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """display a HTML page"""
+    states = storage.all("State")
+    return render_template("9-states.html", states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """display a HTML page"""
+    states = storage.all("State")
+    for state in states.values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html", state=None)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
