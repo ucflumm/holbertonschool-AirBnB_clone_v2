80fe1f1bc16ac54e6b6f2db1b98d328a50b97fd2
ucflumm <ucflumm@gmail.com>
2023-12-16 12:46:20 +1100

    Fix storage.all() argument in states_list() function

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -32,7 +32,7 @@ def teardown_db(exception):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """display a HTML page"""
-    states = storage.all(State)
+    states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
 
