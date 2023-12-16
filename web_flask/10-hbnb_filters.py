57507eaee92b71c66f85de85a8ae67ab7dce8138
ucflumm <ucflumm@gmail.com>
2023-12-16 15:06:11 +1100

    Refactor code to fix import errors and update variable names

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -7,20 +7,19 @@
 """
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
+
+
 app = Flask(__name__)
 
 
 @app.route('/hbnb_filters', strict_slashes=False)
 def hbnb_filters():
     """Display a HTML page like 6-index.html"""
-    states = storage.all(State)
-    cities = storage.all(City)
-    amenities = storage.all(Amenity)
-    return render_template('10-hbnb_filters.html',
-                           states=states, cities=cities, amenities=amenities)
+    states = storage.all("State")
+    cities = storage.all("City")
+    amenities = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
 
 
 @app.teardown_appcontext
